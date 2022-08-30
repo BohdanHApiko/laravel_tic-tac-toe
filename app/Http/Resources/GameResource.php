07b648ceb123d4cc2fc257a8dedef73ffa0d3938
Bohdan H <bohdan.h@apiko.com>
2022-08-30 14:52:46 +0300

    Single game resource class added

---
+++ app/Http/Resources/GameResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GameResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'board' => $this->board_state,
+            'score' => [
+                'x' => $this->scoreX,
+                'o' => $this->scoreO
+            ],
+            'currentTurn' => $this->current_turn,
+            'victory' => $this->winner
+        ];
+    }
+}
