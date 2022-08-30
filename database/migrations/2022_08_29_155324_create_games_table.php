dbef983a79d4e76acf0632481317da6b893c6b64
Bohdan H <bohdan.h@apiko.com>
2022-08-30 13:16:34 +0300

    Game table migration added

---
+++ database/migrations/2022_08_29_155324_create_games_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->json('board_state');
+            $table->char('current_turn', 1)->nullable();
+            $table->char('winner', 1)->nullable();
+            $table->smallInteger('scoreX')->default(0);
+            $table->smallInteger('scoreO')->default(0);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
